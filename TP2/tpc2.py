89c4ff5522f42fdeb05c36136c8bb0c244ba75df
a107158 <a107158@uminho.pt>
2024-10-11 10:09:45 +0100

    Add files via upload

---
+++ TP2/tpc2.py
@@ -0,0 +1,22 @@
+print("Pensa em um número entre 0 e 100, e eu vou tentar adivinhar!")
+
+mínimo = 0
+máximo = 100
+t = 0
+
+while True:
+    adivinha = (mínimo + máximo) // 2
+    t = t + 1
+    print(f"Eu acho que é: {adivinha}")
+
+    r = input("Digite 'm' se for maior, 'n' se for menor ou 'c' se acertei: ")
+
+    if r == 'c' :
+        print(f"Eu acertei! O número é {adivinha}. Necessitei de {t} tentativas.")
+        break
+    elif r == 'm' :
+        mínimo = adivinha + 1
+    elif r == 'n' :
+       máximo = adivinha - 1
+    else:
+        print("Resposta inválida. Por favor, use 'm', 'n' ou 'c'.")
