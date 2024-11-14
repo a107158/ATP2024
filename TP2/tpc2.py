281aee1fb1e58fa332e093f08e98045fd37ee7be
a107158 <a107158@uminho.pt>
2024-11-14 10:42:37 +0000

    Update tpc2.py

--- TP2/tpc2.py
+++ TP2/tpc2.py
@@ -3,20 +3,21 @@ print("Pensa em um número entre 0 e 100, e eu vou tentar adivinhar!")
 mínimo = 0
 máximo = 100
 t = 0
+acertou = False 
 
-while True:
+while not acertou:
     adivinha = (mínimo + máximo) // 2
     t = t + 1
     print(f"Eu acho que é: {adivinha}")
 
-    r = input("Digite 'm' se for maior, 'n' se for menor ou 'c' se acertei: ")
+    r = input("Introduza 'm' se for maior, 'n' se for menor ou 'c' se acertei: ")
 
-    if r == 'c' :
+    if r == 'c':
         print(f"Eu acertei! O número é {adivinha}. Necessitei de {t} tentativas.")
-        break
-    elif r == 'm' :
+        acertou = True  
+    elif r == 'm':
         mínimo = adivinha + 1
-    elif r == 'n' :
-       máximo = adivinha - 1
+    elif r == 'n':
+        máximo = adivinha - 1
     else:
         print("Resposta inválida. Por favor, use 'm', 'n' ou 'c'.")
